cb2ba370922296d57614c4ca05958f4aa1659ab8
Deltric <d3ltr1c@gmail.com>
2022-04-23 16:41:06 -0500

    Command registry

--- src/main/kotlin/dev/deltric/seed/Seed.kt
+++ src/main/kotlin/dev/deltric/seed/Seed.kt
@@ -1,9 +1,13 @@
 package dev.deltric.seed
 
+import com.mojang.brigadier.CommandDispatcher
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
 import net.kyori.adventure.platform.fabric.FabricServerAudiences
 import net.minecraft.server.MinecraftServer
+import net.minecraft.server.command.ServerCommandSource
+
 
 class Seed : ModInitializer {
     companion object  {
@@ -13,6 +17,10 @@ class Seed : ModInitializer {
     override fun onInitialize() {
         ServerLifecycleEvents.SERVER_STARTING.register(ServerLifecycleEvents.ServerStarting { server: MinecraftServer ->
             adventure = FabricServerAudiences.of(server)
+        })
+
+        CommandRegistrationCallback.EVENT.register(CommandRegistrationCallback { dispatcher: CommandDispatcher<ServerCommandSource>, dedicated: Boolean ->
+            if (!dedicated) return@CommandRegistrationCallback
         })
     }
 }
